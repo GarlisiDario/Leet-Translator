e98a68e7ea1a668e249c71286cd04b4cf78a8708
DarioGarlisi <dariogarlisi@gmail.com>
2024-09-19 18:26:12 +0200

    Add project files.

---
+++ Test/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Test
+{
+    public partial class MainPage : ContentPage
+    {
+        LeetTranslator translator = new LeetTranslator();
+        double degree;
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+           string leet =  translator.ToLeet(UserEntry.Text, degree);
+            if (leet != null) 
+            {
+                UserLbl.Text = leet;
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            degree = (double)e.NewValue;
+        }
+    }
+
+}
